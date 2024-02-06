62ce3672dd9c3f6aed5cf03e1104a8144b65f670
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2024-02-06 10:43:31 +0530

    added logic to imageService Model.

--- Swift/TableViewWithStaticData/TableViewWithStaticData/Model/ImageService.swift
+++ Swift/TableViewWithStaticData/TableViewWithStaticData/Model/ImageService.swift
@@ -8,4 +8,16 @@
 import Foundation
 
 class ImageService {
+    var pictures = [String]()
+    func fetchImages() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
 }
