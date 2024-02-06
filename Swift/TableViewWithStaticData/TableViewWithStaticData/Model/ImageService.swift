f106261ca30c6126af40919c36a3092b3fc052f8
Kiran Kumar <kirankumar@Kirans-MacBook-Pro.local>
2024-02-06 10:40:00 +0530

    tableview with Static data practice.

---
+++ Swift/TableViewWithStaticData/TableViewWithStaticData/Model/ImageService.swift
@@ -0,0 +1,11 @@
+//
+//  ImageService.swift
+//  TableViewWithStaticData
+//
+//  Created by Kiran Kumar on 06/02/24.
+//
+
+import Foundation
+
+class ImageService {
+}
---
+++ Swift/TableViewWithStaticData/TableViewWithStaticData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewWithStaticData
+//
+//  Created by Kiran Kumar on 06/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
